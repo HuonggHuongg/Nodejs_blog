763cd56664d6657f906eaa939002a2578aa301e6
DESKTOP-GV2LFTN\Hương  Hương <huongghuongg9900@gmail.com>
2021-10-31 14:00:25 +0700

    first commit

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController{
+    index(reg, res){
+        res.render('news');
+    }
+
+    //GET (/news: slug)
+    show(reg, res){
+        res.send('News detail!!');
+    }
+}
+
+module.exports = new NewsController;
